0bb6e735153f6c2d38a758ea2d3034032fa70450
Laith <laith@ovestlabs.com.au>
2017-09-22 14:08:20 +1000

    First commit.

---
+++ Source/Win2dFlicker/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Windows.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+
+namespace Win2dFlicker
+{
+	public sealed partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private readonly Random _random = new Random();
+
+		private byte CreateRandomByte()
+		{
+			return (byte)(_random.NextDouble() * 0xFF);
+		}
+
+		private Color CreateRandomColor()
+		{
+			var red = CreateRandomByte();
+			var green = CreateRandomByte();
+			var blue = CreateRandomByte();
+			var color = Color.FromArgb(0xFF, red, green, blue);
+
+			return color;
+		}
+
+		private void OnRegionsInvalidated(CanvasVirtualControl sender, CanvasRegionsInvalidatedEventArgs e)
+		{
+			foreach (var region in e.InvalidatedRegions)
+			{
+				using (var ds = sender.CreateDrawingSession(region))
+				{
+					var color = CreateRandomColor();
+					ds.Clear(color);
+				}
+			}
+		}
+	}
+}
